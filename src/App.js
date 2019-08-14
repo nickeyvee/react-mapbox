9772031a4aae8b21d424f45801955a0b281e6e91
Nick Vorraso <nvorraso2020@gmail.com>
2019-08-14 17:48:51 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, { useState } from 'react';
-import mbxClient from 'mapbox-gl'
 // import axios from 'axios';
 
 import MapContainer from './MapContainer'
 import Sidebar from './Sidebar'
 
 import './App.css';
- 
-// const baseClient = mbxClient({ accessToken: 'pk.eyJ1Ijoibmlja2V5dmVlIiwiYSI6ImNqejdwb3FoZTBoMzEzYm1qOHlwaTh4eTMifQ.szHP6I8_HT6BtVPVsAbrag' })
 
 function App() {
   const coord = useState([-0.481747846041145, 51.3233379650232])
 
   const getCoord = v => {
     console.log(v)
-    // baseClient.forewardGeocode(v, function(err, data, res) {
-      // console.log(err, data)
-      // data is the geocoding result as parsed JSON
-      // res is the http response, including: status, headers and entity properties
-    // });
   }
   return (
     <div className="App">
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import mapboxgl from 'mapbox-gl'
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
 import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
-
+// mapboxgl.accessToken = 'pk.eyJ1Ijoibmlja2V5dmVlIiwiYSI6ImNqejdwb3FoZTBoMzEzYm1qOHlwaTh4eTMifQ.szHP6I8_HT6BtVPVsAbrag'
 const Map = ReactMapboxGl({
   accessToken: 'pk.eyJ1Ijoibmlja2V5dmVlIiwiYSI6ImNqejdwb3FoZTBoMzEzYm1qOHlwaTh4eTMifQ.szHP6I8_HT6BtVPVsAbrag'
 });
 
+// https://docs.mapbox.com/mapbox-gl-js/example/mapbox-gl-geocoder/
+
 function MapContainer (props) {
+  // const map = new mapboxgl.Map({
+  //   container: 'map',
+  //   style: 'mapbox://styles/mapbox/streets-v9',
+  //   center: props.coord,
+  //   zoom: 13
+  // })
+  map.addControl(new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken, mapboxgl
+  }))
   return (
+    // <div id="map">
+    //   {/* PLACEHOLDER */}
+    // </div>
     <Map
       style="mapbox://styles/mapbox/streets-v9"
       containerStyle={{
